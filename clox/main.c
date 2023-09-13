74c74807ed1852c6351aa069ad4f3afeac725402
Chet Aldrich <chet.aldrich@joinroot.com>
2023-09-12 23:08:54 -0400

    chapter 15

--- clox/main.c
+++ clox/main.c
@@ -3,15 +3,27 @@
 #include "debug.h"
 #include "vm.h"
 
+void writeConstant(Chunk* chunk, Value value);
+
+void writeConstant(Chunk* chunk, Value value) {
+    int constant = addConstant(chunk, value);
+    writeChunk(chunk, OP_CONSTANT, 123);
+    writeChunk(chunk, constant, 123);
+}
+
 int main(int argc, const char *argv[]) {
     initVM();
 
     Chunk chunk;
     initChunk(&chunk);
 
-    int constant = addConstant(&chunk, 1.2);
-    writeChunk(&chunk, OP_CONSTANT, 123);
-    writeChunk(&chunk, constant, 123);
+    writeConstant(&chunk, 1);
+    writeConstant(&chunk, 3);
+    writeChunk(&chunk, OP_ADD, 123);
+    writeConstant(&chunk, 4);
+
+    writeChunk(&chunk, OP_DIVIDE, 123);
+    writeChunk(&chunk, OP_NEGATE, 123);
     writeChunk(&chunk, OP_RETURN, 123);
     interpret(&chunk);
 
@@ -19,4 +31,3 @@ int main(int argc, const char *argv[]) {
     freeChunk(&chunk);
     return 0;
 }
-
